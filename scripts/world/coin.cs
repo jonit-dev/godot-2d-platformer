71ac845d52225aa1e67ee4d5b4f3adbdcbceddda
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-07-03 16:39:23 -0700

    coin destroy itself on pickup

---
+++ scripts/world/coin.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class coin : Area2D
+{
+
+	private AudioStreamPlayer audioPlayer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+
+
+		// Connect the 'body_entered' signal to the 'OnBodyEntered' method using a Callable
+		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+	}
+
+	// Method to handle the event when a body enters the area
+	private void OnBodyEntered(Node body)
+	{
+		// Implement what should happen when a body enters this area
+		GD.Print("A body entered: ", body.Name);
+
+		if (body.Name == "Player")
+		{
+			// destroy itself
+			QueueFree();
+		}
+
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
